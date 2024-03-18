c76ac95ecfe309a550c9f6b674b959f620b0d865
Tatu Soininen <taatusti@gmail.com>
2024-03-18 10:47:25 +0200

    Basic REST controller for testing

---
+++ src/main/java/fi/soininen/tatu/spring6security/configuration/WebSecurityConfig.java
@@ -0,0 +1,5 @@
+package fi.soininen.tatu.spring6security.configuration;
+
+public class WebSecurityConfig {
+
+}
---
+++ src/main/java/fi/soininen/tatu/spring6security/controllers/TestController.java
@@ -0,0 +1,14 @@
+package fi.soininen.tatu.spring6security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!\n";
+    }
+
+}
